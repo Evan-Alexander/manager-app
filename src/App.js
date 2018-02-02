c275b8bf5edad3cc3c8aff7427a152ee1d82d37b
Jason Brown <jasontbrown99@gmail.com>
2018-02-02 08:58:22 -0800

    connect to firebase; import common components; create blank login form w/ no functionality

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'API KEY',
+      authDomain: 'manager-3ca7c.firebaseapp.com',
+      databaseURL: 'https://manager-3ca7c.firebaseio.com',
+      projectId: 'manager-3ca7c',
+      storageBucket: '',
+      messagingSenderId: '864360134471'
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
         <View>
-          <Text>
-            Hello!
-          </Text>
+
+          <LoginForm />
         </View>
       </Provider>
     );
