a574fb0d40691e7193a37955703ca2a61149b82e
Jason Brown <jasontbrown99@gmail.com>
2018-02-13 10:50:26 -0800

    navigate back to employeeList after adding an employee

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
 import {
   EMPLOYEE_UPDATE
 } from './types';
@@ -7,4 +9,14 @@ export const employeeUpdate = ({ prop, value }) => {
      type: EMPLOYEE_UPDATE,
      payload: { prop, value }
    };
+};
+
+export const employeeCreate = ({ name, phone, shift }) => {
+  const { currentUser } = firebase.auth();
+
+  return () => {
+    firebase.database().ref(`/users/${currentUser.uid}/employees`)
+    .push({ name, phone, shift })
+    .then(() => Actions.pop());
+  };
 };
