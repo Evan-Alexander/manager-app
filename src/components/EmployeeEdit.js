31157770715d0020c1250d4e24f004677e7dc5b0
jb <jasontbrown99@gmail.com>
2018-02-14 10:38:41 -0800

    updated records saving to firebase

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import EmployeeForm from './EmployeeForm';
-import { employeeUpdate } from '../actions';
+import { employeeUpdate, employeeSave } from '../actions';
 import { Card, CardSection, Button } from './common';
 
 class EmployeeEdit extends Component {
@@ -14,7 +14,7 @@ class EmployeeEdit extends Component {
 
   onButtonPress() {
     const { name, phone, shift } = this.props;
-    console.log(name, phone, shift);
+    this.props.employeeSave({ name, phone, shift, uid: this.props.employee.uid });
   }
 
   render() {
@@ -39,5 +39,5 @@ const mapStateToProps = (state) => {
 };
 
 export default connect(mapStateToProps, {
-  employeeUpdate
+  employeeUpdate, employeeSave
 })(EmployeeEdit);
